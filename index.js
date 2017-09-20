90e80aa641402246ac0563340072d507fa0ee3e3
Andrei Sierkov <andrei.sierkov@railsreactor.com>
2017-09-20 03:09:07 +0300

    Testing DB

--- index.js
+++ index.js
@@ -9,11 +9,11 @@ app.set('port', (process.env.PORT || 5000));
 const ENV = process.env.TELEGRAM_CONTEST_ENV || 'development';
 
 const TBot = require('node-telegram-bot-api');
-const telegram = new TBot(process.env.TELEGRAM_CONTEST_TOKEN, { polling: true });
-
-telegram.on('text', (message) => {
-  telegram.sendMessage(message.chat.id, 'test');
-});
+// const telegram = new TBot(process.env.TELEGRAM_CONTEST_TOKEN, { polling: true });
+//
+// telegram.on('text', (message) => {
+//   telegram.sendMessage(message.chat.id, 'test');
+// });
 
 const { Client } = require('pg');
 const dbConfig = ENV === 'development'
@@ -31,10 +31,8 @@ const client = new Client(dbConfig);
 app.get('/', function (req, res) {
   client.connect()
     .then(() => {
-      res.send('Connected successfully');
       client.query('SELECT * FROM winners WHERE TRUE')
         .then((response) => {
-          res.send('DB test success');
           res.send(JSON.stringify(response));
           client.end();
         })
@@ -53,14 +51,14 @@ app.listen(app.get('port'), function () {
   console.log('Contest app is running on port', app.get('port'));
 });
 
-// client.connect().then(() => {
-//   client.query('SELECT * FROM winners WHERE TRUE').then((response) => {
-//     console.log(response);
-//   });
-//   client.query({
-//     text: 'INSERT INTO winners(name, email) VALUES($1, $2)',
-//     values: ['brianc', 'brian.m.carlson@gmail.com'],
-//   }).then(() => {
-//     client.end();
-//   });
-// });
+client.connect().then(() => {
+  client.query('SELECT * FROM winners WHERE TRUE').then((response) => {
+    console.log('select success');
+  });
+  client.query({
+    text: 'INSERT INTO winners(name, email) VALUES($1, $2)',
+    values: ['brianc', 'brian.m.carlson@gmail.com'],
+  }).then(() => {
+    client.end();
+  });
+});
