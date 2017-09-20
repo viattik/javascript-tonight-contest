436f52cdba9e6fefe97729712f32614aee17ac04
Andrei Sierkov <andrei.sierkov@railsreactor.com>
2017-09-20 03:11:55 +0300

    Testing DB

--- index.js
+++ index.js
@@ -6,8 +6,6 @@ const app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-const ENV = process.env.TELEGRAM_CONTEST_ENV || 'development';
-
 const TBot = require('node-telegram-bot-api');
 // const telegram = new TBot(process.env.TELEGRAM_CONTEST_TOKEN, { polling: true });
 //
@@ -16,17 +14,14 @@ const TBot = require('node-telegram-bot-api');
 // });
 
 const { Client } = require('pg');
-const dbConfig = ENV === 'development'
-  ? {
-    host: process.env.TELEGRAM_CONTEST_DB_HOST,
-    database: process.env.TELEGRAM_CONTEST_DB_DATABASE,
-    user: process.env.TELEGRAM_CONTEST_DB_USER,
-    password: process.env.TELEGRAM_CONTEST_DB_PASSWORD,
-    port: '5432',
-    ssl: true,
-  }
-  : undefined;
-const client = new Client(dbConfig);
+const client = new Client({
+  host: process.env.TELEGRAM_CONTEST_DB_HOST,
+  database: process.env.TELEGRAM_CONTEST_DB_DATABASE,
+  user: process.env.TELEGRAM_CONTEST_DB_USER,
+  password: process.env.TELEGRAM_CONTEST_DB_PASSWORD,
+  port: '5432',
+  ssl: true,
+});
 
 app.get('/', function (req, res) {
   client.connect()
